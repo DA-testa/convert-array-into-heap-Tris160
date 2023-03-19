74146708168a419afbb89313a8374b3e61500b42
Tris160 <125840531+Tris160@users.noreply.github.com>
2023-03-19 22:04:37 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,36 +1,69 @@
-# python3
+#python
+import sys
 
-def sift_down(i, n, data):
-    max_index = i
-    l = 2 * i + 1
-    if l < n and data[l] < data[max_index]:
-        max_index = l
-    r = 2 * i + 2
-    if r < n and data[r] < data[max_index]:
-        max_index = r
-    if i != max_index:
-        data[i], data[max_index] = data[max_index], data[i]
-        swaps.append((i, max_index))
-        sift_down(max_index, n, data)
 
-def build_heap(data):
-    n = len(data)
+def build_heap(data, n):
+    swaps = []
+ 
+    def heapify(i):
+        nonlocal swaps
+
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and data[left] < data[smallest]:
+            smallest = left
+
+        if right < n and data[right] < data[smallest]:
+            smallest = right
+
+        if smallest != i:
+            data[i], data[smallest] = data[smallest], data[i]
+            swaps.append((i, smallest))
+            heapify(smallest)
+
     for i in range(n // 2, -1, -1):
-        sift_down(i, n, data)
-    return data
+        heapify(i)
+
+    return swaps
+
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
-    assert len(data) == n
+  
+    input_method = input().strip()
+    if input_method.startswith("I"):
+        # Input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif input_method.startswith("F"):
+        print("File path:")
+        file_name = input().strip()
+        file_path = "./tests/"
+        if "a" not in file_name:
+            with open(file_path + file_name, mode="r") as file:
+                n = int(file.readline())
+                data = list(map(int, file.readline().split()))
+        else:
+            exit()
+    else:
+        exit()
 
-    global swaps
-    swaps = []
-    build_heap(data)
-    
+
+    assert len(data) == n, "Entered length must be the same as the actual length of the array"
+
+
+    swaps = build_heap(data, n)
+
+
+    assert len(swaps) <= 4 * n, "The number of swaps exceeds the limit."
+
+  
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
+
 if __name__ == "__main__":
     main()
+
