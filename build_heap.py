3bc5adbcb75e77cdee1f60594e8c5d1865720417
Tris160 <125840531+Tris160@users.noreply.github.com>
2023-03-19 22:00:12 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,46 +1,36 @@
 # python3
 
+def sift_down(i, n, data):
+    max_index = i
+    l = 2 * i + 1
+    if l < n and data[l] < data[max_index]:
+        max_index = l
+    r = 2 * i + 2
+    if r < n and data[r] < data[max_index]:
+        max_index = r
+    if i != max_index:
+        data[i], data[max_index] = data[max_index], data[i]
+        swaps.append((i, max_index))
+        sift_down(max_index, n, data)
+
 def build_heap(data):
-    swaps = []
     n = len(data)
-
     for i in range(n // 2, -1, -1):
-        sift_down(i, data, swaps)
-
-    return swaps
-
-
-def sift_down(i, data, swaps):
-    n = len(data)
-    min_idx = i
-
-    l = 2*i + 1
-    if l < n and data[l] < data[min_idx]:
-        min_idx = l
-
-    r = 2*i + 2
-    if r < n and data[r] < data[min_idx]:
-        min_idx = r
-
-    if i != min_idx:
-        swaps.append((i, min_idx))
-        data[i], data[min_idx] = data[min_idx], data[i]
-        sift_down(min_idx, data, swaps)
-
+        sift_down(i, n, data)
+    return data
 
 def main():
-    n, c = input().split()
-    n = int(n)
+    n = int(input())
     data = list(map(int, input().split()))
-
     assert len(data) == n
 
-    swaps = build_heap(data)
-
+    global swaps
+    swaps = []
+    build_heap(data)
+    
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
